d31d07666e0eef9fc0a58a775d6139c884af50a9
iwatamomoka <c0b2202091@edu.teu.ac.jp>
2023-04-25 14:51:21 +0900

    練習2

--- 練習.py
+++ 練習.py
@@ -3,6 +3,15 @@ import sys
 
 import pygame as pg
 
+
+delta = {
+        pg.K_UP: (0, -1), # 移動量の値であり、(横,縦)
+        pg.K_DOWN: (0, +1),
+        pg.K_LEFT: (-1, 0),
+        pg.K_RIGHT: (+1, 0),
+        }
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -10,6 +19,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rect = kk_img.get_rect() # ?? 
+    kk_rect.center = 900, 400
     
     bb_img = pg.Surface((20, 20))  # 半径20の正方形
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 赤色で10,10の位置に半径10の円を描く
@@ -28,8 +39,14 @@ def main():
                 return 0
 
         tmr += 1
+
+        key_list = pg.key.get_pressed()
+        for k, mv in delta.items(): # mv = 移動量
+            if key_list[k]:
+                kk_rect.move_ip((mv))
+
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rect)
         bb_rect.move_ip(vx, vy) #移動部分
         screen.blit(bb_img, bb_rect) #表示
 
