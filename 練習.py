eab3744a619de52cff21dda36944ee7cdf044c76
iwatamomoka <c0b2202091@edu.teu.ac.jp>
2023-04-25 14:32:33 +0900

    練習2

--- 練習.py
+++ 練習.py
@@ -10,12 +10,17 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    tmr = 0
+    
     bb_img = pg.Surface((20, 20))  # 半径20の正方形
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 赤色で10,10の位置に半径10の円を描く
     bb_img.set_colorkey((0, 0, 0))  # 円の黒い線を消した
     x, y = random.randint(0,1600),random.randint(0,900)  # 縦,横の乱数
     screen.blit(bb_img, [x, y])  #bb_img(赤い円)をぶりっと
+    vx, vy = +1, +1 #横、縦速度
+    bb_rect = bb_img.get_rect() # ?? 
+    bb_rect.center = x, y # 爆弾の中心座標に x, y を ??
+    tmr = 0
+
 
     while True:
         for event in pg.event.get():    # これがないとゲームが終わらないから必ず必要
@@ -25,7 +30,10 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img, [x, y])  #bb_img(赤い円)をぶりっと.(x,y)でもいい
+        bb_rect.move_ip(vx, vy) #移動部分
+        screen.blit(bb_img, bb_rect) #表示
+
+        # screen.blit(bb_img, [x, y])  #bb_img(赤い円)をぶりっと.(x,y)でもいい
 
 
         pg.display.update()
